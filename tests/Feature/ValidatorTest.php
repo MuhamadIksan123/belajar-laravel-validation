c6ada1659e68f13461c555492d95390c8d7e61d2
Muhamad Iksan <muhamadiksan351@gmail.com>
2023-12-07 18:45:46 +0700

    setup

---
+++ tests/Feature/ValidatorTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Validator;
+use Tests\TestCase;
+
+class ValidatorTest extends TestCase
+{
+    public function testValidator()
+    {
+        $data = [
+            "username" => "admin",
+            "password" => "123456"
+        ];
+
+        $rules = [
+            "username" => "required",
+            "password" => "required"
+        ];
+
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator);
+
+        self::assertTrue($validator->passes());
+        self::assertFalse($validator->fails());
+    }
+
+    public function testValidatorInvalid()
+    {
+        $data = [
+            "username" => "",
+            "password" => ""
+        ];
+
+        $rules = [
+            "username" => "required",
+            "password" => "required"
+        ];
+
+        $validator = Validator::make($data, $rules);
+        self::assertNotNull($validator);
+
+        self::assertFalse($validator->passes());
+        self::assertTrue($validator->fails());
+    }
+}
